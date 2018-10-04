3f68dbca8bbcd3b5450a25f27c90c5587779a22c
Petersen <apetersen@skylinetechnologies.com>
2018-10-04 11:21:21 -0500

    added action definitions filter and column title

--- src/sitescripts/components/actionDefinitions/ActionDefinitions.tsx
+++ src/sitescripts/components/actionDefinitions/ActionDefinitions.tsx
@@ -2,28 +2,46 @@ import * as React from 'react';
 import { ActionDefinition } from '../../data/interfaces';
 import { Droppable, DroppableStateSnapshot, Draggable } from 'react-beautiful-dnd';
 import ActionDefinitionItem from './ActionDefinitionItem';
-
-const getClassNames = (snapshot:DroppableStateSnapshot) => {
+import { TextField } from "office-ui-fabric-react/lib/TextField";
+import "./ActionDefinitions.scss";
+const getColumnClassNames = (snapshot:DroppableStateSnapshot) => {
     return [
-        "action-definitions",
+        "action-definitions-column",
         "column",
         snapshot.isDraggingOver ? "dragging" : ""
     ].filter(c => c).join(" ");
 }
 export default class ActionDefinitions extends React.PureComponent<ActionDefinitionsProps, {}> {
+    state = {
+        filter: "",
+    }
+    onFilterChange = (newValue) => {
+        this.setState({ filter: newValue })
+    }
     render() {
         return (
-            <Droppable droppableId="action-definitions" isDropDisabled={true}>
-                {(provided, snapshot) => {
-                    return (
-                        <div className={getClassNames(snapshot)} ref={provided.innerRef}>
-                            {this.props.actionDefinitions.map((action, index) => (
-                                <ActionDefinitionItem key={action.verb} action={action} index={index} />
-                            ))}
-                        </div>
-                    )
-                }}
-            </Droppable>
+            <div className='action-definitions'>
+                <div className='title'>Available Actions</div>
+                <div className='filter'>
+                    <TextField value={this.state.filter} onChanged={this.onFilterChange} placeholder="Filter..." />
+                </div>
+                <Droppable droppableId="action-definitions" isDropDisabled={true}>
+                    {(provided, snapshot) => {
+                        return (
+                            <div className={getColumnClassNames(snapshot)} ref={provided.innerRef}>
+                                {this.props.actionDefinitions
+                                    .filter(action => {
+                                        return action.title.toLowerCase().indexOf(this.state.filter.toLowerCase()) > -1
+                                    })
+                                    .map((action, index) => (
+                                        <ActionDefinitionItem key={action.verb} action={action} index={index} />
+                                    ))
+                                }
+                            </div>
+                        )
+                    }}
+                </Droppable>
+            </div>
         );
     }
 }
